3b892a6de6f4b9dffb6fa867c404fce27411d4b3
gnadais-42 <guilhermenadaispinho@gmail.com>
2026-03-03 17:26:08 +0000

    minor changes

--- Python_8/ex1/loading.py
+++ Python_8/ex1/loading.py
@@ -19,12 +19,23 @@ def main() -> None:
     if missing:
         print("Missing dependencies:", missing, end="\n\n")
         print("To install these dependencies you can "
-              "install them manually like:")
+              "install them manually using pip like:")
         print("python3 -m pip install ", end="")
         for package in missing:
             print(package + " ", end="")
-        print("\n\n Or you can install everything in requirements.txt like:")
-        print("python3 -m pip install -r requirements.txt")
+        print("\n\nOr you can install everything in requirements.txt like:")
+        print("python3 -m pip install -r requirements.txt\n")
+        print("To install them using poetry you can do:\n"
+              "poetry init\n"
+              "Then follow the instructions or:\n"
+              "poetry add ", end="")
+        for package in missing:
+            print(package + " ", end="")
+        print("\nThen run it with:\n"
+              "poetry run python3 loading.py\n")
+        print("If you already have a valid pyproject.toml file you can just run it with poetry")
+        print("Just make sure you're in a safe environment!")
+        
 
         
 
